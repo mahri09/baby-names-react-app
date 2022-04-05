865e58796edb45c03b312edcfa7ef62500b131cc
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-04-05 07:14:29 +0100

    changed favorites text

--- src/FavoriteNames.js
+++ src/FavoriteNames.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const FavoriteNames = ({ favoriteNames }) => {
   return (
-    <div className="content ">
-      Favorites:
+    <div className="content favorite-content">
+      Click to Add Favorite Baby Names:
       {favoriteNames.map((favName, index) => (
         <button key={index} className={favName.className}>
           {favName.name}
