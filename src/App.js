93b3fab1d94d00cb89e8b3a554ca443bbe3df6e4
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-04-05 05:42:15 +0100

    completed baby-name app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import Header from "./Header.js";
 import nameData from "./data/nameData.json";
@@ -10,16 +11,34 @@ function App() {
   const [filteredNames, setFilteredName] = useState([]);
   const [searchField, setSearchField] = useState("");
   const [favoriteNames, setFavoriteNames] = useState([]);
+  const [filteredByGender, setFilteredByGender] = useState("");
 
   useEffect(() => {
-    const filteredNames = names.filter((baby) => {
+    let filteredNamesBySearch = names.filter((baby) => {
       return baby.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    setFilteredName(filteredNames);
+    setFilteredName(filteredNamesBySearch);
   }, [searchField]);
+
+  useEffect(() => {
+    const filteredNamesByGender = names.filter((baby) => {
+      if (filteredByGender === "baby-boy") {
+        return baby.sex === "m";
+      } else if (filteredByGender === "baby-girl") {
+        return baby.sex === "f";
+      } else {
+        return baby;
+      }
+    });
+    setFilteredName(filteredNamesByGender);
+  }, [filteredByGender]);
+
   return (
     <div className="container App">
-      <Header handleSearch={setSearchField} />
+      <Header
+        setSearchField={setSearchField}
+        setFilteredByGender={setFilteredByGender}
+      />
       <FavoriteNames favoriteNames={favoriteNames} />
       <Content
         favoriteNames={favoriteNames}
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 
-const Header = ({ handleSearch }) => {
-  const handleChange = (e) => {
-    handleSearch(e.target.value);
+const Header = ({ setSearchField, setFilteredByGender }) => {
+  const handleSearchInputChange = (e) => {
+    setSearchField(e.target.value);
   };
+
+  const handleSetFilteredByGender = (e) => {
+    setFilteredByGender(e.target.alt);
+  };
+
   return (
     <div className="search-content">
       <input
@@ -11,23 +17,28 @@ const Header = ({ handleSearch }) => {
         type="search"
         placeholder="Search"
         aria-label="Search"
-        onChange={handleChange}
+        onChange={handleSearchInputChange}
       />
-      <img
-        className="search-icon"
-        src="https://cdn-icons.flaticon.com/png/512/2102/premium/2102832.png?token=exp=1648765631~hmac=b09bd8645ff5268723ac86a6ff9ffc6e"
-        alt="baby image"
-      />
-      <img
-        className="search-icon"
-        src="https://cdn-icons.flaticon.com/png/512/3320/premium/3320112.png?token=exp=1648765618~hmac=956912fc57f1985cb701189b665980f0"
-        alt="baby image"
-      />
-      <img
-        className="search-icon"
-        src="https://cdn-icons-png.flaticon.com/512/2867/2867024.png"
-        alt="baby image"
-      />
+      <div className="icon-box">
+        <img
+          onClick={handleSetFilteredByGender}
+          className="search-icon"
+          src="https://cdn-icons.flaticon.com/png/512/2102/premium/2102832.png?token=exp=1648765631~hmac=b09bd8645ff5268723ac86a6ff9ffc6e"
+          alt="all-babies"
+        />
+        <img
+          onClick={handleSetFilteredByGender}
+          className="search-icon"
+          src="https://cdn-icons.flaticon.com/png/512/3320/premium/3320112.png?token=exp=1648765618~hmac=956912fc57f1985cb701189b665980f0"
+          alt="baby-girl"
+        />
+        <img
+          onClick={handleSetFilteredByGender}
+          className="search-icon"
+          src="https://cdn-icons-png.flaticon.com/512/2867/2867024.png"
+          alt="baby-boy"
+        />
+      </div>
     </div>
   );
 };
